1477de5a04c25a7cdd5b4007816a82faceae5038
Count Infinity <countinfinity@easyday.dev>
2024-01-03 19:02:48 -0700

    Split base attribute and race, add totals

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -28,8 +28,33 @@ export class Player {
 
     getAbilityValue(ability: string) {
         if (ability == "strength") {
-            console.log("Strenght");
+            console.log("Strength");
             let race_mod = this.race?.strength_mod ?? 0;
+            return race_mod + this.abilities[ability]?.value;
+        }
+        else if (ability == "dexterity") {
+            console.log("dexterity");
+            let race_mod = this.race?.dexterity_mod ?? 0;
+            return race_mod + this.abilities[ability]?.value;
+        }
+        else if (ability == "constitution") {
+            console.log("Strength");
+            let race_mod = this.race?.constitution_mod ?? 0;
+            return race_mod + this.abilities[ability]?.value;
+        }
+        else if (ability == "intelligence") {
+            console.log("intelligence");
+            let race_mod = this.race?.intelligence_mod ?? 0;
+            return race_mod + this.abilities[ability]?.value;
+        }
+        else if (ability == "wisdom") {
+            console.log("wisdom");
+            let race_mod = this.race?.wisdom_mod ?? 0;
+            return race_mod + this.abilities[ability]?.value;
+        }
+        else if (ability == "charisma") {
+            console.log("charisma");
+            let race_mod = this.race?.charisma_mod ?? 0;
             return race_mod + this.abilities[ability]?.value;
         }
         return 0;
--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -48,17 +48,27 @@ export class PlayerService {
   {
     let player = this.playerSubject.getValue();
     player.race=race;
-
-    let total_strength = player.getAbilityValue("strength");
-
-    if ( total_strength > 18) {
-      player.discretionary += (total_strength-18);
-      player.abilities['strength'].value -= (total_strength-18);
-    }
+    
+    this.checkAttributeValue("strength");
+    this.checkAttributeValue("dexterity");
+    this.checkAttributeValue("constitution");
+    this.checkAttributeValue("intelligence");
+    this.checkAttributeValue("wisdom");
+    this.checkAttributeValue("charisma");
       
     
     this.playerSubject.next(player);
     
+  }
+
+  checkAttributeValue(attributeName:string) {
+    let player = this.playerSubject.getValue();
+    let total_ability = player.getAbilityValue(attributeName);
+    if ( total_ability > 18) {
+      player.discretionary += (total_ability-18);
+      player.abilities[attributeName].value -= (total_ability-18);
+    }
+
   }
 
   
